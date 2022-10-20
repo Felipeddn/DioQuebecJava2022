215a821f173e061847694cc7cc8994b6a383f307
Felipeddn <felipe_ddn_@hotmail.com>
2022-10-20 04:30:57 -0300

    "Aprendendo sobre a IDE Intellij parte 2"

--- _m03-DominandoALinguagemDeProgramacaoJava/_c01-DominandoIDEsJava/_p05-Intellij/primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ _m03-DominandoALinguagemDeProgramacaoJava/_c01-DominandoIDEsJava/_p05-Intellij/primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -7,9 +7,37 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O problemas dos três corpos", 300);
+        System.out.println(livro1);
         /*int numero1 = 2;
         int numero2 = 3;
         System.out.println("Hello, World! " + (numero2 + numero1));*/
 
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numeroDePaginas;
+
+    public Livro(String nome, Integer numeroDePaginas){
+        this.nome = nome;
+        this.numeroDePaginas = numeroDePaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numeroDePaginas=" + numeroDePaginas +
+                '}';
     }
 }
